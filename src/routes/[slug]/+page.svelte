594454076184d1b63b93a06b3f6ae5e0aafe889f
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2022-12-02 11:50:39 +1300

    Bookshop live editing in CloudCannon

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,12 +1,36 @@
 <script>
-	import { Bookshop } from "@bookshop/sveltekit-bookshop";
+	import { onDestroy, onMount } from "svelte";
+	import {
+		Bookshop,
+		trackBookshopLiveData
+	 } from "@bookshop/sveltekit-bookshop";
+	import {
+		onCloudCannonChanges,
+		stopCloudCannonChanges,
+	} from "@cloudcannon/svelte-connector";
+	import { site_title } from '@data/site.json'
 
 	export let data;
 
-	let content_blocks = data.data.content_blocks;
+	let pageDetails = data.data;
+
+	let title = `${site_title}${pageDetails.title ? ` | ${pageDetails.title}` : ''}`;
+
+	onMount(async () => {
+		onCloudCannonChanges(
+			(newProps) => (pageDetails = trackBookshopLiveData(newProps))
+		);
+	});
+	onDestroy(async () => {
+		stopCloudCannonChanges();
+	});
+
 </script>
 
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 
 <div>
-	<Bookshop shared="page" {content_blocks} />
+	<Bookshop shared="page" content_blocks={pageDetails.content_blocks} />
 </div>
