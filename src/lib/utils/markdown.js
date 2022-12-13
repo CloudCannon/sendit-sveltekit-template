f2e0e2e03d63b507c57a57f5e90075cd7bc0f2e7
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2022-12-14 11:18:48 +1300

    Fixed markdownify sometimes receiving undefined content

--- src/lib/utils/markdown.js
+++ src/lib/utils/markdown.js
@@ -4,11 +4,13 @@ const md = new MarkdownIt({ html: true });
 
 
 export function markdownify(content) {
+	content = content || '';
 	const contentHtml = md.renderInline(content);
 	return contentHtml;
 }
 
 export function getContentMeta(content) {
+	content = content || '';
 	const WORDS_PER_MINUTE = 183;
 	const text = content.trim();
 	const words = text.match(/(^|\s+|\/|-)\S+/ig);
